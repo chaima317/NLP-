7e514297437642e83e3318488d9862e6b21c20de
chaima <abdellalichaimaa@gmail.com>
2022-11-22 22:19:09 +0100

    gana

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 ex= Extract()
 ex.wordcloud.generate(ex.text)
 fig, ax = plt.subplots(1,1, figsize = (9,6))
+print("GANA")
 # add interpolation = bilinear to smooth things out
 plt.imshow(ex.wordcloud, interpolation='bilinear')
 # and remove the axis
